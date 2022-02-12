2d483f95d10e254b453d8983e4753b0a801cb6b7
shawnmiller124 <83323320+shawnmiller124@users.noreply.github.com>
2022-02-11 22:06:11 -0500

    Create gestionLED.h

---
+++ PROJET_S2/src/gestionLED.h
@@ -0,0 +1,20 @@
+/**
+ * @file gestionLED.h
+ * @author Shawn Miller (mils2203@usherbrooke.ca)
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-11
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef GESTIONLED_H_
+#define GESTIONLED_H_
+
+void initGestionLED();
+
+
+
+
+#endif /* GESTIONLED_H_ */
