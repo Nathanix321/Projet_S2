1c82664ada9b9b07f165ae74789438e1a56ced47
shawnmiller124 <83323320+shawnmiller124@users.noreply.github.com>
2022-02-12 00:12:15 -0500

    Update main.cpp

--- PROJET_S2/src/main.cpp
+++ PROJET_S2/src/main.cpp
@@ -8,33 +8,52 @@
  * @author Shawn Miller (mils2203@usherbrooke.ca)
  * @author Shawn Miller (mils2203@usherbrooke.ca)
  * @author Shawn Miller (mils2203@usherbrooke.ca)
- *        
- * @brief 
+ *
+ * @brief
  * @version 0.1
  * @date 2022-02-11
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
 
 #include <Arduino.h>
-#include "Timer.h"
 #include "accelerometre.h"
 #include "memory.h"
+
+/**     Etat Transition          **/
+// variable
+bool commandeValid;
+int id;
+// constante
+enum etatTransition
+{
+  START,
+  COMMANDE,
+  LED,
+  BOMBE,
+  END,
+
+} EtatTransition;
+
+// fonction
+bool etatTransitionModule(char rxData);
 
 void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(115200);
+
+  // etatTransition
+  EtatTransition = START;
+  commandeValid = false;
+  id = 0;
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
 }
-
-
-
 
 /**
  * Possibiliter de reception
@@ -45,7 +64,7 @@ void loop()
  *  -<5> //accelerometre
  *  -<6,/a determiner/> //LED
  *  -<7,ON/OFF> //moteur vibrant
- *  
+ *
  * Possibiliter de d'envoie
  *  -<1,wire_analog_value> //wire
  *  -<2,padluck_analog_Xvalue,padluck_analog_Yvalue> //Padluck
@@ -53,3 +72,70 @@ void loop()
  *  -<4,keypad_digital_value> //Keypad
  *  -<5,accelerometre_analog_Xvalue,accelerometre_analog_Yvalue,accelerometre_analog_Zvalue> //accelerometre
  */
+
+void serialEvent()
+{
+  while (Serial.available())
+  {
+    etatTransitionModule((char)Serial.read());
+  }
+}
+
+bool etatTransitionModule(char rxData)
+{
+  switch (EtatTransition)
+  {
+  case START:
+    if(rxData == '<'){
+      EtatTransition = COMMANDE;
+    }
+    else{
+      EtatTransition = START;
+      Serial.print("ERROR OF COMMUNICATION\n\r");
+      commandeValid = false;
+    }
+    break;
+
+  case COMMANDE:
+    if(('1' <= rxData) && (rxData <= '7')){
+      id = rxData - '0';
+      Serial.print(rxData);
+
+      if((1 <= id) && (id <= 5)){
+        EtatTransition = END;
+      }
+      else if(id == 6){
+        EtatTransition = LED;
+      }
+            else if(id == 7){
+        EtatTransition = BOMBE;
+      }
+    }
+    else{
+      EtatTransition = END;
+      Serial.print("COMMANDE NOT AVAILABLE\n\r");
+      commandeValid = false;
+    }
+    break;
+
+  case LED:
+    /* code */
+    break;
+
+  case BOMBE:
+    /* code */
+    break;
+
+  case END:
+    if(rxData == '>'){
+      commandeValid = true;
+    }
+    else{
+      Serial.print("ERROR OF COMMUNICATION\n\r");
+      commandeValid = false;
+    }
+    EtatTransition = START;
+    break;
+  }
+  return 0;
+}
