0c885455e6114e309289fcbed534185e5a240bba
shawnmiller124 <83323320+shawnmiller124@users.noreply.github.com>
2022-02-11 22:06:05 -0500

    Create accelerometre.c

---
+++ PROJET_S2/src/accelerometre.c
@@ -0,0 +1,12 @@
+/**
+ * @file accelerometre.c
+ * @author Shawn Miller (mils2203@usherbrooke.ca)
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-11
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+ #include "accelerometre.h"
+
