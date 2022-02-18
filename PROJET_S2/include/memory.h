a7de0d3dc91324b8ec12af9bdf21f537dd8910fe
shawnmiller124 <83323320+shawnmiller124@users.noreply.github.com>
2022-02-17 22:03:55 -0500

    Create memory.h

---
+++ PROJET_S2/include/memory.h
@@ -0,0 +1,28 @@
+/**
+ * @file memory.h
+ * @author Shawn Miller (mils2203@usherbrooke.ca)
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-11
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef MEMORY_H_
+#define MEMORY_H_
+
+#include <Arduino.h>
+
+/**
+ * @brief initialise les pins pour le module MEMORY
+ * 
+ */
+void memoryInit();
+
+
+
+
+
+
+
+#endif /* MEMORY_H_ */
