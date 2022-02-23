da4da562e2b2db7ad85d210c8ed766d655c4e077
shawnmiller124 <83323320+shawnmiller124@users.noreply.github.com>
2022-02-23 18:46:52 -0500

    Update accelerometre.hpp

--- PROJET_S2/lib/accelerometre/accelerometre.hpp
+++ PROJET_S2/lib/accelerometre/accelerometre.hpp
@@ -1,32 +1,36 @@
 /**
  * @file accelerometre.h
  * @author Shawn Miller (mils2203@usherbrooke.ca)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2022-02-11
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
 #ifndef ACCELEROMETRE_H_
 #define ACCELEROMETRE_H_
 
 #include <Arduino.h>
 
+class Accelerometre
+{ // The class
+private:
+  uint8_t pinX;
+  uint8_t pinY;
+  uint8_t pinZ;
 
-class Accelerometre {       // The class
-  private:
-    uint8_t pinX;
-    uint8_t pinY;
-    uint8_t pinZ;
+  void _accelerometreTimerInit();
 
-  public:             // Access specifier
-    Accelerometre();
-    Accelerometre(uint8_t A_xPin, uint8_t A_yPin, uint8_t A_zPin);
-    ~Accelerometre();
-    int getX_value();
-    int getY_value();
-    int getZ_value();
+public: // Access specifier
+  Accelerometre();
+  Accelerometre(uint8_t A_xPin, uint8_t A_yPin, uint8_t A_zPin);
+  ~Accelerometre();
+  int getX_value();
+  int getY_value();
+  int getZ_value();
+  bool getStateFlag();
+  void setStateFlag(bool state);
 };
 
 #endif /* ACCELEROMETRE_H_ */
