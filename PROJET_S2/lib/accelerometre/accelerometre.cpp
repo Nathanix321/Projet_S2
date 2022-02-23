1383664293e4f84db9c7ebbe89b0782303212bb3
shawnmiller124 <83323320+shawnmiller124@users.noreply.github.com>
2022-02-23 18:46:48 -0500

    Update accelerometre.cpp

--- PROJET_S2/lib/accelerometre/accelerometre.cpp
+++ PROJET_S2/lib/accelerometre/accelerometre.cpp
@@ -11,34 +11,43 @@
 
 #include "accelerometre.hpp"
 
+bool flagAccelerometre;
+uint16_t comptTimer;
+
 /**
  * @brief Construct a new Accelerometre:: Accelerometre object
- * 
+ *
  */
-Accelerometre::Accelerometre(){
-
+Accelerometre::Accelerometre()
+{
 }
 
 /**
  * @brief Construct a new Accelerometre:: Accelerometre object
- * 
- * @param A_xPin 
- * @param A_yPin 
- * @param A_zPin 
+ *
+ * @param A_xPin
+ * @param A_yPin
+ * @param A_zPin
  */
 Accelerometre::Accelerometre(uint8_t A_xPin, uint8_t A_yPin, uint8_t A_zPin)
 {
-    pinX = A_xPin;
-    pinY = A_yPin;
-    pinZ = A_zPin;
+  pinX = A_xPin;
+  pinY = A_yPin;
+  pinZ = A_zPin;
 
-    pinMode(pinX, INPUT);
-    pinMode(pinY, INPUT);
-    pinMode(pinZ, INPUT);
+  pinMode(pinX, INPUT);
+  pinMode(pinY, INPUT);
+  pinMode(pinZ, INPUT);
+
+  flagAccelerometre = false;
+  comptTimer = 0;
+
+  _accelerometreTimerInit();
+  Serial.println("Init accelerometre");
 }
 /**
  * @brief Destroy the Accelerometre:: Accelerometre object
- * 
+ *
  */
 Accelerometre::~Accelerometre()
 {
@@ -46,31 +55,81 @@ Accelerometre::~Accelerometre()
 
 /**
  * @brief get voltage of the X axe
- * 
- * @return voltage 
+ *
+ * @return voltage
  */
 int Accelerometre::getX_value()
 {
-    return analogRead(pinX);
+  return analogRead(pinX);
 }
 
 /**
  * @brief get voltage of the Y axe
- * 
- * @return voltage 
+ *
+ * @return voltage
  */
 int Accelerometre::getY_value()
 {
-    return analogRead(pinY);
+  return analogRead(pinY);
 }
 
 /**
  * @brief get voltage of the Z axe
- * 
- * @return voltage 
+ *
+ * @return voltage
  */
 int Accelerometre::getZ_value()
 {
-    return analogRead(pinZ);
-    
+  return analogRead(pinZ);
+}
+
+/**
+ * @brief
+ *
+ * @return true
+ * @return false
+ */
+bool Accelerometre::getStateFlag()
+{
+  return flagAccelerometre;
+}
+
+/**
+ * @brief
+ *
+ * @param state
+ */
+void Accelerometre::setStateFlag(bool state)
+{
+  flagAccelerometre = state;
+}
+
+/**
+ * @brief initialisation du timer0 pour l'accelerometre
+ *
+ */
+void Accelerometre::_accelerometreTimerInit()
+{
+  comptTimer = 0;
+  TCCR0A |= (1 << WGM01);
+  TCCR0B |= (1 << CS01) + (1 << CS00); // F_CPU/64 Hz -> 4us
+  OCR0A = 250;                         // compare match a tous les 1ms
+  TIMSK0 = (1 << OCIE0A);              // active interupt A
+}
+
+/**
+ *	@brief Fonction d'interuption du TIMER 0 qui compte jusq'a 100ms
+ *	@param compare le vecteur OCR0A
+ *	@param n/a
+ *	@return n/a
+ */
+ISR(TIMER0_COMPA_vect)
+{
+  comptTimer++;
+  if (comptTimer >= 100)
+  {
+    comptTimer -= 100;
+    flagAccelerometre = true;
+  }
+  //Serial.println(comptTimer);
 }
