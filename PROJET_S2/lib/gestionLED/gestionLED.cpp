c9e3280dd579ad524c147ba8e82d14d77d30af9e
shawnmiller124 <83323320+shawnmiller124@users.noreply.github.com>
2022-02-24 12:01:06 -0500

    Create gestionLED.cpp

---
+++ PROJET_S2/lib/gestionLED/gestionLED.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file gestionLED.c
+ * @author Shawn Miller (mils2203@usherbrooke.ca)
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-11
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include "gestionLED.hpp"
+
+/**
+ * @brief initialisation des LED
+ * 
+ */
+void initGestionLED(){
+
+}
