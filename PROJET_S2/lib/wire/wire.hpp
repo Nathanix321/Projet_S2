580a34d1c03ca0ebbc4e5dc3833d9bca8d56ef72
shawnmiller124 <83323320+shawnmiller124@users.noreply.github.com>
2022-03-10 13:13:47 -0500

    Create wire.hpp

---
+++ PROJET_S2/lib/wire/wire.hpp
@@ -0,0 +1,37 @@
+/**
+ * @file memory.h
+ * @author Shawn Miller (mils2203@usherbrooke.ca)
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-11
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef WIRE_H_
+#define WIRE_H_
+
+#include <Arduino.h>
+
+class Wire 
+{
+public:
+    Wire();
+    Wire();
+    ~Wire();
+
+;
+    
+
+private:
+
+
+};
+
+
+
+
+
+
+
+#endif /* WIRE_H_ */
