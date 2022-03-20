9155215516fcfbfcad966e86fe321c2bc998c012
justhoude <justin.houde@usherbrooke.ca>
2022-03-20 14:23:59 -0400

    Keypad v2

--- PROJET_S2/lib/keypad/keypad.cpp
+++ PROJET_S2/lib/keypad/keypad.cpp
@@ -10,43 +10,10 @@
  * 
  */
 
-
-
-
-
-/**
- * 
- *  Revoir class, l'arduino doit seulement detecter un changement d'état et l'envoyer au PC, donc les méthodes ne corespond pas a ce qui est demandé 
- * 
- **/
-
-
-
-
-
-
 #include "KeyPad.hpp"
-
-
-// Pourquoi tu les declares dans le .cpp et le .hpp?????? 
-
-uint8_t switchPin1;
-uint8_t switchPin2;
-uint8_t switchPin3;
-uint8_t switchPin4;
-//Fonction qui permet de générer une matrice de fils, aucune validation. Basé sur la fonction RandomNumberGenerator(). 
-//Prend en paramètres un minimum, maximum, le nombre de chiffre à générer et la matrice de fils initilisée dans le main().
-//Aucun retour, la matrice de fils est modifié directement à l'adresse némoire. 
 
 Keypad :: Keypad(uint8_t switchPin1, uint8_t switchPin2, uint8_t switchPin3, uint8_t switchPin4)
 {
-  int arr[BOUTMAX][STATES]; // le tableau es local au constructeur, donc ne peux pas etre utiliser
-  keypadInit(arr);
-  genmodule(0, 4, BOUTMAX, arr);
-  /*switchPin1 = this->switchPin1; v   c'est l'inverse, le this affect la variable privé de la class et non la variable en parametre
-  switchPin2 = this->switchPin2;
-  switchPin3 = this->switchPin3;
-  switchPin4 = this->switchPin4;*/
   this->switchPin1 = switchPin1;
   this->switchPin2 = switchPin2;
   this->switchPin3 = switchPin3;
@@ -57,107 +24,30 @@ Keypad::~Keypad()
 {
 }
 
-
-/**
-*
-* A quoi sert cette méthode la ???? l'arduino ne genere pas de module
-*
- **/
-void Keypad :: genmodule(const int nMin, const int nMax, const int  nToGenerate, int arr[BOUTMAX][STATES])
+int Keypad :: detecterTouche()
 {
-  int ran_num = 0;
-  for (int i = 0; i < nToGenerate;)
-  {
-    ran_num = rand()%(nMax-nMin) + nMin;
-    if (ran_num!=arr[1][0] && ran_num!=arr[0][0] && ran_num!=arr[2][0] && ran_num!=arr[3][0])
-    {
-		arr[i][0]=ran_num;
-		i++;
-	}
-  }
+   if (switchPin1 == HIGH)
+   {
+           return 1;
+   }
+
+   else if (switchPin2 == HIGH)
+   {
+           return 2;
+   }
+
+   else if (switchPin3 == HIGH)
+   {
+           return 3;
+   }
+
+   else if (switchPin4 == HIGH)
+   {
+           return 4;
+   }
+   
+   else
+        return 0;
+
+
 }
-
-
-/**
-*
-* A quoi sert cette méthode la ???? 
-*
- **/
-void Keypad :: keypadInit(int arr[BOUTMAX][STATES])
-{
-	for (int i=0; i<BOUTMAX;i++)
-	{
-		arr[i][1]=ON;
-		arr[i][0]=5;
-	}
-}
-
-int Keypad :: Reussi(int arr[BOUTMAX][STATES])
-{
-	int total=0;
-	for (int i=0;i<BOUTMAX;i++)
-	{
-		if (arr[i][1]==1)
-		{
-			total++;
-		}
-	}
-	return total;
-}
-
-/**
-*
-* A quoi sert cette méthode la ???? l'arduino ne genere pas de module
-*
- **/
-void Keypad :: detecterTouche(int arr[BOUTMAX][STATES])
-{
-    if (digitalRead(switchPin1) == HIGH)
-    {
-        if (arr[1][1] == tour && arr[1][1] == 0)
-	{
-		arr[1][1]=1;
-        tour++;
-	}
-        else
-            status = GAMEOVER;
-    }
-    
-    if (digitalRead(switchPin2) == HIGH)
-    {
-        if (arr[2][1] == tour  && arr[2][1] == 0)
-	{
-		arr[2][1]=1;
-        tour++;
-	}
-        else
-            status = GAMEOVER;
-    }
-
-    if (digitalRead(switchPin3) == HIGH)
-    {
-        if (arr[3][1] == 0 && arr[3][1] == 0)
-	{
-		arr[3][1]=1;
-        tour++;
-	}
-        else
-            status = GAMEOVER;
-    }
-
-    if (digitalRead(switchPin4) == HIGH)
-    {
-        if (arr[4][1] == 0 && arr[4][1] == 0)
-	{
-		arr[4][1]=1;
-        tour++;
-	}
-        else
-            status = GAMEOVER;
-    }
-    
-	if (Reussi(arr) == 4)
-	    status = RIGHT;
-}
-
-
--- PROJET_S2/lib/keypad/keypad.hpp
+++ PROJET_S2/lib/keypad/keypad.hpp
@@ -10,22 +10,14 @@
  * 
  */
 
+#ifndef KEYPAD_H_
+#define KEYPAD_H_
+
 //H22-S2-PROJET-P09
 #include <Arduino.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-//#include <windows.h>
-//Constantes de dimensions pour la matrice de fils (4 fils, 3 états : couleur, coupé ou intact, bon ou mauvais).
-#define BOUTMAX 4
-#define STATES 3
-
-//Constantes d'états pour la matrice de fils(ON/OFF : coupé et intact, RIGHT/WRONG : bon et mauvais).
-#define ON 0
-#define OFF -1
-#define RIGHT 1
-#define WRONG -1
-#define GAMEOVER 2
 
 class Keypad
 {
@@ -34,17 +26,14 @@ public:
     Keypad();
     ~Keypad();
 
-    void genmodule(const int nMin, const int nMax, const int  nToGenerate, int arr[BOUTMAX][STATES]);
-    void keypadInit(int arr[BOUTMAX][STATES]);
-    int Reussi(int arr[BOUTMAX][STATES]);
-    void detecterTouche(int arr[BOUTMAX][STATES]);
+    int detecterTouche();
 
 private:
-    int tour = 0;
-    int status = WRONG;
     uint8_t switchPin1;
     uint8_t switchPin2;
     uint8_t switchPin3;
     uint8_t switchPin4;
 
 };
+
+#endif /* KEYPAD_H_ */
